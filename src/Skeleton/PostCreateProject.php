ca7b32b46b567cb2e21e7192fb86bac91d0a44d1
Pascal Jean-Francois <jeffpascal96@gmail.com>
2022-11-25 12:30:55 +0200

    Update PostCreateProject.php

--- src/Skeleton/PostCreateProject.php
+++ src/Skeleton/PostCreateProject.php
@@ -8,17 +8,59 @@ class PostCreateProject
 {
     public static function run(Event $event): void
     {
-        self::pinVolta();
-        self::runNpmInstall($event);
-        self::installNpmPackages($event);
-        self::installFrameworkStylePackage($event);
-        self::reconfigureWebpack($event);
-        self::createAssets($event);
-        self::reconfigureApplication($event);
-        self::cleanupFiles($event);
-        self::cleanup($event);
-        self::runNpmBuild($event);
-        self::dumpInitialTranslations($event);
+        // self::pinVolta();
+        // self::runNpmInstall($event);
+        // self::installNpmPackages($event);
+        // self::installFrameworkStylePackage($event);
+        // self::reconfigureWebpack($event);
+        // self::createAssets($event);
+        // self::reconfigureApplication($event);
+        self::postCreateProject($event);
+        // self::cleanupFiles($event);
+        // self::cleanup($event);
+        // self::runNpmBuild($event);
+        // self::dumpInitialTranslations($event);
+    }
+
+
+    private static function postCreateProject(Event $event)
+    {
+        $root = dirname(__DIR__);
+        $io = $event->getIO();
+        $name = $io->ask('Package Name: ');
+
+        if($name === '') {
+            throw new RuntimeException('Name can not be empty');
+        }
+
+        $autoNamespace = str_replace(' ', '\\\\', $name);
+        $autoAlias = str_replace(' ', '-', strtolower($name));
+
+        $namespace = $io->ask(sprintf('Namespace [%s]: ', $autoNamespace)) ?: $autoNamespace;
+        $alias = $io->ask(sprintf('Package Alias [%s]: ', $autoAlias)) ?: $autoAlias;
+
+        file_put_contents(
+            $root . '/README.md',
+            self::getReplacedFileContents($root . '/README.md', $name, $alias, $namespace)
+        );
+        file_put_contents(
+            $root . '/composer.json',
+            preg_replace('/(,\s+"post-create-project-cmd".*\])/Us', '',
+                self::getReplacedFileContents($root . '/composer.json', $name, $alias, $namespace)
+            )
+        );
+
+        unlink($root . '/tests/LibrarySkeleton.php');
+        unlink(__FILE__);
+    }
+
+    private static function getReplacedFileContents($file, $name, $alias, $namespace) {
+        return strtr(file_get_contents($file), [
+            'Library Skeleton' => $name,
+            'library-skeleton' => $alias,
+            '"Zero\\\\"' => '"Zero\\\\' . $namespace . '\\\\"',
+            '"ZeroTest\\\\"' => '"ZeroTest\\\\' . $namespace . '\\\\"'
+        ]);
     }
 
     private static function runNpmInstall(Event $event): void
